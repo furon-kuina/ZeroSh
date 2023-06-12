45eb88996557358e05e9c70106a4017da042803c
Furon-Kuina <tenma.edamura@gmail.com>
2023-06-12 09:30:15 +0900

    feat: wait and output the input

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
+use std::io::{stdin, stdout, Write};
+
 fn main() {
     shell_loop()
 }
 
 fn shell_loop() {
-    loop {}
+    while let Some(line) = shell_read_line() {
+        println!("{}", line)
+    }
+}
+
+fn shell_read_line() -> Option<String> {
+    print!("> ");
+    stdout().flush().unwrap();
+    let mut result = String::new();
+    match stdin().read_line(&mut result) {
+        Ok(size) => {
+            if size == 0 {
+                None
+            } else {
+                let result = result.trim_end();
+                Some(result.to_string())
+            }
+        }
+        Err(e) => {
+            eprintln!("{}", e);
+            None
+        }
+    }
 }
